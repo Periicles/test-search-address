3ed5d20198015fb254f964fb8fb727f1bfb84265
Periicles <landescrew.scoot@gmail.com>
2025-12-23 16:20:49 +0100

    refactor: fixed added forgotten type

--- src/types/TApiResponse.ts
+++ src/types/TApiResponse.ts
@@ -1,6 +1,14 @@
+import type { IAddress } from '../interfaces/IAddress';
+
+export type TFeature = {
+    type: 'Feature';
+    properties: IAddress;
+};
+
 export type TApiResponse = {
     type: 'FeatureCollection';
     version: string;
+    features: Array<TFeature>;
     attribution: string;
     licence: string;
     query?: string;
